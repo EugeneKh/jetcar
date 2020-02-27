731d0086843a918b8389c299351a049aad71c4d1
EugeneKh <eugenekh@outlook.com>
2020-02-28 01:47:49 +0500

    Delete npy

--- recognition/config.py
+++ recognition/config.py
@@ -9,9 +9,9 @@ class jet:
     learning_rate: float = 0.001 # Скорость обучения η
     epoch_nums: int = 10
 
-    @staticmethod
+    @staticmethod # для интерактивной консоли
     def reload():
         import sys
         if "config" in  sys.modules:
             del(sys.modules["config"])
-        from config import jet
+#        from config import jet
